6d8cd214bbaba2c8cfb6cc4fede9744985a49344
ddraj8960 <ddraj8960@gmail.com>
2026-03-06 10:14:20 +0530

    UC1: Application Entry & Welcome Message

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,25 +1,8 @@
-import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-
-        String reversed = "";
-
-        // Reverse the string using for loop
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-
-        // Compare original and reversed string
-        if (input.equals(reversed)) {
-            System.out.println("The given string is a Palindrome");
-        } else {
-            System.out.println("The given string is NOT a Palindrome");
-        }
-
-        sc.close();
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
